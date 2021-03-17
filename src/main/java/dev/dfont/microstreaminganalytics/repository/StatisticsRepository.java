86f1fe9e8f592e8b9facbcf36f0a080c6e09d5c7
Daniel Font <danielfontsuarez@gmail.com>
2021-03-17 14:05:21 +0100

    Remove unnecessary find method.

--- src/main/java/dev/dfont/microstreaminganalytics/repository/StatisticsRepository.java
+++ src/main/java/dev/dfont/microstreaminganalytics/repository/StatisticsRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface StatisticsRepository  extends MongoRepository<Statistics, String> {
 
-//    /Statistics findBText(String text);
 
 }
