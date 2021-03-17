1957e05774fecac304720700059abb31bc8e240c
Daniel Font <danielfontsuarez@gmail.com>
2021-03-17 14:00:33 +0100

    Add Javadoc, logger and fix the size of the data to process.

--- src/main/java/dev/dfont/microstreaminganalytics/config/Receiver.java
+++ src/main/java/dev/dfont/microstreaminganalytics/config/Receiver.java
@@ -14,6 +14,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * The type Receiver called by RabbitMqConfig.
+ * We use this Receiver to process messages from a RabbitMQ queue, we decide to group this values in groups of 100 elements
+ * in order to process the statistic analysis before push the data into our mongoDB repository.
+ *
+ * Keep in mind that this is POC, un real life there has no sense to process this data in this way, it's better to perform
+ * the statistic analysis getting the data already saved in the DB.
+ */
 public class Receiver {
 
     public static final String RECEIVE_METHOD_NAME = "receiveMessage";
@@ -38,13 +47,13 @@ public class Receiver {
         Device device = MessageEncoder.createPojoFromMessage(message);
         doubleList.add(device.getDatastreams().get(0).getDatapoints().get(0).value);
 
-        if (doubleList.size() == 10){
+        if (doubleList.size() == 100){
             Statistics statistics = StatisticsUtils.proccessData(doubleList);
+            logger.info("Saving data into DB");
             statisticsRepository.save(statistics);
             doubleList.clear();
         }
 
-        logger.info("My value model from message "+ device.getDatastreams().get(0).getDatapoints().get(0).value);
 
     }
 }
