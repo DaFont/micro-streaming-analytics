28efc34058d96a0a888bf0c0de262617ba2267dd
Daniel Font <danielfontsuarez@gmail.com>
2021-03-17 14:04:22 +0100

    Add Javadoc.

--- src/main/java/dev/dfont/microstreaminganalytics/utils/StatisticsUtils.java
+++ src/main/java/dev/dfont/microstreaminganalytics/utils/StatisticsUtils.java
@@ -3,54 +3,73 @@ package dev.dfont.microstreaminganalytics.utils;
 
 import dev.dfont.microstreaminganalytics.model.Statistics;
 import org.apache.commons.math3.stat.StatUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 
+
+/**
+ * The type Statistics utils to process groups of data using Apache Common Stat library to get statistic analysis values.
+ */
 public class StatisticsUtils {
 
+    private static final Logger logger = LoggerFactory.getLogger(StatisticsUtils.class);
 
+    /**
+     * Proccess data statistics.
+     *
+     * @param doubleList the double list
+     * @return the statistics
+     */
     public static Statistics proccessData(List<Double> doubleList) {
 
 
         Statistics statistics = new Statistics();
 
-        //media, mediana, moda, desviación típica, cuartiles, valor máximo y valor mínimo.
+        //Current timestamp
+        statistics.setTimestamp(Instant.now());
+
+        //Mapping data from list to array of doubles to use Apache Commons Stat library
         double[] doubles = doubleList.stream().mapToDouble(Double::doubleValue).toArray();
 
         //Max
-        System.out.println(StatUtils.max(doubles));
+        logger.info("Calculate statistic analysis");
+
+        logger.info(String.valueOf(StatUtils.max(doubles)));
         statistics.setMax(StatUtils.max(doubles));
 
         //Min
-        System.out.println(StatUtils.min(doubles));
+        logger.info(String.valueOf(StatUtils.min(doubles)));
         statistics.setMin(StatUtils.min(doubles));
 
         //Mode
-        System.out.println(Arrays.toString(StatUtils.mode(doubles)));
+        logger.info(Arrays.toString(StatUtils.mode(doubles)));
 
         //Media
-        System.out.println(StatUtils.mean(doubles));
+        logger.info(String.valueOf(StatUtils.mean(doubles)));
         statistics.setMean(StatUtils.mean(doubles));
 
 
         //Varianza
-        System.out.println(StatUtils.variance(doubles));
+        logger.info(String.valueOf(StatUtils.variance(doubles)));
         statistics.setVariance(StatUtils.variance(doubles));
 
         //Desviacion tipica
-        System.out.println(Math.sqrt(StatUtils.variance(doubles)));
+        logger.info(String.valueOf(Math.sqrt(StatUtils.variance(doubles))));
         statistics.setStd(Math.sqrt(statistics.getVariance()));
 
 
-        System.out.println(StatUtils.percentile(doubles, 25));
+        logger.info(String.valueOf(StatUtils.percentile(doubles, 25)));
         statistics.setQuartile25(StatUtils.percentile(doubles, 25));
 
-        System.out.println(StatUtils.percentile(doubles, 50));
+        logger.info(String.valueOf(StatUtils.percentile(doubles, 50)));
         statistics.setQuartile50(StatUtils.percentile(doubles, 50));
 
 
-        System.out.println(StatUtils.percentile(doubles, 75));
+        logger.info(String.valueOf(StatUtils.percentile(doubles, 75)));
         statistics.setQuartile75(StatUtils.percentile(doubles, 75));
 
         return statistics;
